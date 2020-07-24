753abdc446d44909216c19d3c6c39f6d43f5609d
Seong <sryoo6@gatech.edu>
2020-07-23 21:51:38 -0400

    Get relevant information printed out

--- block-render/sched-data.php
+++ block-render/sched-data.php
@@ -40,18 +40,106 @@ function appia_get_meta( $id, $meta_name ) {
   $the_string = get_post_meta( $id, $meta_name, true );
   $cleaned = utf8_encode( $the_string );
   $the_object = json_decode( $cleaned, true );
-  $the_data = $the_object['data'];
+  $the_data = array();
+  if ( isset( $the_object['data'] ) ) {
+    $the_data = $the_object['data'];
+  }
   return $the_data;
+}
+
+function appia_get_table_name( $tables, $index ) {
+  $name = '';
+  if ( count( $tables ) > $index ) {
+    $the_object = $tables[$index];
+    appia_get_name( $the_object );
+  }
+  return $name;
+}
+
+function appia_get_name( $table ) {
+  $name = '';
+  if ( isset( $table['name'] ) ) {
+    $name = $table['name'];
+  }
+  return $name;
+}
+
+function appia_sessions( $all_sessions, $slot_num, $track_num ) {
+  $the_sessions = array();
+  if ( isset( $all_sessions[$slot_num] ) ) {
+    $slot = $all_sessions[$slot_num];
+    if ( isset( $slot[$track_num] ) ) {
+      $the_sessions = $slot[$track_num];
+    }
+  }
+  return $the_sessions;
 }
 
 function appia_sched_data_block_render( $attributes ) {
   global $post;
   $id = $post->ID;
+
   $sessions = appia_get_meta( $id, 'post_sched_meta_sessions' );
-  var_dump( $sessions );
-  $markup = 'no';
-  $markup .= $id;
-  // $markup .= $sessionsJSON;
+  $tracks = appia_get_meta( $id, 'post_sched_meta_tracks' );
+  $slots = appia_get_meta( $id, 'post_sched_meta_slots' );
+  
+  $markup = '';
+
+  $markup .= '<div class="tracks-container">';
+    $markup .= '<div class="track-offset"></div>';
+
+    foreach( $tracks as $track ) {
+      $markup .= '<div class="track">';
+        $markup .= appia_get_name( $track );
+      $markup .= '</div>';
+    }
+
+  $markup .= '</div>'; // end tracks-container
+
+  $markup .= '<div class="slots-container">';
+    foreach( $slots as $slot_index=>$slot ) {
+      $markup .= '<div class="slot">';
+        $markup .= '<div class="slot-name">';
+          $markup .= appia_get_name( $slot );
+        $markup .= '</div>'; // end slot-name
+
+        $markup .= '<div class="slot-dropzones-container">';
+          foreach( $tracks as $track_index=>$track ) {
+            $stored = appia_sessions( $sessions,
+                                      $slot_index,
+                                      $track_index );
+            foreach( $stored as $sesh ) {
+              $sesh_id = intval( $sesh );
+              $sesh_name = get_the_title( $sesh_id );
+              $sesh_speakers = get_post_meta( $sesh_id, 'post_sesh_meta_speakers', true );
+              $sesh_desc = get_post_meta( $sesh_id, 'post_sesh_meta_desc', true );
+              $sesh_link = get_post_meta( $sesh_id, 'post_sesh_meta_link', true );
+
+                $markup .= '<div class="session">';
+                  $markup .= '<div class="session-name">';
+                    $markup .= $sesh_name;
+                  $markup .= '</div>';
+
+                  $markup .= '<div class="session-speakers">';
+                    $markup .= $sesh_speakers;
+                  $markup .= '</div>';
+
+                  $markup .= '<div class="session-desc">';
+                    $markup .= $sesh_desc;
+                  $markup .= '</div>';
+
+                  $markup .= '<div class="session-link">';
+                    $markup .= $sesh_link;
+                  $markup .= '</div>';
+                $markup .= '</div>';
+
+            }
+            // var_dump( $sessions );
+
+          }
+        $markup .= '</div>';
+      $markup .= '</div>';
+    }
   
   return $markup;
 }
